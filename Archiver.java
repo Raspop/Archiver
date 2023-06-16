e88e7e36afbe22ca45dcbd28c20c16bc2c91f212
Roman <3654004@gmail.com>
2023-06-16 14:39:51 +0500

    Initial commit

---
+++ Archiver.java
@@ -0,0 +1,38 @@
+package com.javarush.task.task31.task3110;
+
+import com.javarush.task.task31.task3110.command.ExitCommand;
+import com.javarush.task.task31.task3110.exception.WrongZipFileException;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.file.Paths;
+
+public class Archiver {
+
+    public static void main(String[] args) throws Exception {
+        Operation operation = null;
+        while (operation != Operation.EXIT){
+            try {
+                operation = askOperation();
+                CommandExecutor.execute(operation);
+            }catch (WrongZipFileException e){
+                ConsoleHelper.writeMessage("Вы не выбрали файл архива или выбрали неверный файл.");
+            }catch (Exception e) {
+                ConsoleHelper.writeMessage("Произошла ошибка. Проверьте введенные данные.");
+            }
+        }
+
+    }
+    public static Operation askOperation() throws IOException {
+        ConsoleHelper.writeMessage("Выберите операцию:\n" +
+                "0 - упаковать файлы в архив\n" +
+                "1 - добавить файл в архив\n" +
+                "2 - удалить файл из архива\n" +
+                "3 - распаковать архив\n" +
+                "4 - просмотреть содержимое архива\n" +
+                "5 - выход");
+        return Operation.values()[ConsoleHelper.readInt()];
+    }
+
+}
